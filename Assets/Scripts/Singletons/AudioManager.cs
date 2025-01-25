980161c853f5ae9ce192a76f43e065627e1524d4
Damiano Tagliaferri <ironcutter24@gmail.com>
2025-01-25 20:40:26 +0100

    Add FMOD and bubble SFX

---
+++ Assets/Scripts/Singletons/AudioManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using Utilities;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    private FMOD.Studio.EventInstance _bubbleBounceInstance;
+    
+    [SerializeField] private FMODUnity.EventReference bubbleBounceEvent;
+    [SerializeField] private FMODUnity.EventReference bubbleMergeEvent;
+    [SerializeField] private FMODUnity.EventReference bubblePopEvent;
+
+    private void Start()
+    {
+        _bubbleBounceInstance = FMODUnity.RuntimeManager.CreateInstance(bubbleBounceEvent);
+    }
+
+    public void PlayBubbleMerge() => PlaySound(bubbleMergeEvent);
+    public void PlayPopMerge() => PlaySound(bubblePopEvent);
+    
+    public void PlayBubbleBounce(float pitch)
+    {
+        pitch = Mathf.Clamp01(pitch);
+        _bubbleBounceInstance.setParameterByName("BouncePitch", pitch);
+        _bubbleBounceInstance.start();
+    }
+
+    private static void PlaySound(FMODUnity.EventReference eventRef)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(eventRef);
+    }
+}
